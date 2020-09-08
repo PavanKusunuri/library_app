28adfc67958ecc1175377158b4e9c88d6761b768
Pavan <pavantejakusunuri@gmail.com>
2020-09-08 22:22:56 +0530

    Built django api end point for listing all books in library

--- library_project/urls.py
+++ library_project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include  # new
+from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('books.urls')),  # new
+    path('', include('books.urls')),
+    path('api/', include('api.urls')),
 ]
